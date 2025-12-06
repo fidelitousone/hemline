320e8fa8bab96080591c4e2c221aedafa28a2c98
Jatinder Singh <fidelitousone@gmail.com>
2025-12-06 17:15:21 -0500

    directory structure changes

--- content-script.js
+++
@@ -1,9 +0,0 @@
-var jobTitle = document.getElementsByClassName('job__title');
-if (jobTitle.length != 0) {
-    var primaryElement = jobTitle[0];
-    if (primaryElement instanceof HTMLElement) {
-        var title = primaryElement.innerText;
-        var jobTitle_1 = title.split('\n');
-        console.log(jobTitle_1[0]);
-    }
-}
