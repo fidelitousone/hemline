8502b0ed19e216d51e8617f9fceb8523bf13e551
Jatinder Singh <fidelitousone@gmail.com>
2026-03-05 03:36:30 -0500

    Use tailwind CSS instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
 
 export default defineConfig({
-  plugins: [vue(), crx({ manifest })],
+  plugins: [tailwindcss(), vue(), crx({ manifest })],
 });
