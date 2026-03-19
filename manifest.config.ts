fcb1767786aa70e06eb16a74b97518e0056fecaa
Jatinder Singh <fidelitousone@gmail.com>
2026-03-19 18:03:46 -0400

    Deprecate manifest.json in favor of CRXJS manifest + add Winston logging

---
+++ manifest.config.ts
@@ -0,0 +1,23 @@
+import { defineManifest } from '@crxjs/vite-plugin';
+
+export default defineManifest({
+  manifest_version: 3,
+  name: 'Hemline',
+  description: 'An extension to quickly tailor a resume using AI',
+  version: '0.1.0',
+  permissions: ['activeTab', 'storage'],
+  background: {
+    service_worker: 'src/background.ts',
+    type: 'module',
+  },
+  action: {
+    default_popup: 'index.html',
+    default_icon: 'placeholder.png',
+  },
+  content_scripts: [
+    {
+      matches: ['https://job-boards.greenhouse.io/*'],
+      js: ['src/greenhouse-ats-parsing.ts'],
+    },
+  ],
+});
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import { crx } from '@crxjs/vite-plugin';
-import manifest from './manifest.json';
+import manifest from './manifest.config.ts';
 
 export default defineConfig({
   plugins: [tailwindcss(), vue(), crx({ manifest })],
